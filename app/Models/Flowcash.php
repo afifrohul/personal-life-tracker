1f05cb2893786411cf4041651ec6fe3f2f2a7c59
afifrohul <afifmemyself22@gmail.com>
2025-12-24 15:56:23 +0700

    add flowcash factory

--- app/Models/Flowcash.php
+++ app/Models/Flowcash.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Flowcash extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['flowcash_category_id', 'date', 'amount', 'description', 'type'];
 
     public function flowcashCategory()
---
+++ database/factories/FlowcashFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\FlowcashCategory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Flowcash>
+ */
+class FlowcashFactory extends Factory
+{
+    protected $model = \App\Models\Flowcash::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'flowcash_category_id' => FlowcashCategory::all()->random()->id, // Pilih kategori acak
+            'date' => $this->faker->dateTimeBetween('-2 year', 'now'), // Tanggal dalam 1 tahun terakhir
+            'amount' => $this->faker->randomFloat(2, 50000, 100000), 
+            'description' => $this->faker->sentence(), // Deskripsi acak
+            'type' => $this->faker->randomElement(['income', 'expense']), // Jenis transaksi
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
