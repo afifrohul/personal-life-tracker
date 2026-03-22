ace7dc24de3e9d9e8da966a7d33db3b6e7bc088f
afifrohul <afifmemyself22@gmail.com>
2026-03-22 11:11:45 +0700

    fix typo in footer

--- resources/js/components/footer.tsx
+++ resources/js/components/footer.tsx
@@ -9,8 +9,7 @@ export default function Footer() {
             <div className="flex flex-col items-center justify-between gap-4 p-4 lg:flex-row">
                 <div className="flex items-center justify-center gap-2 lg:justify-start">
                     <p className="text-xs italic">
-                        Built by Afif Rohul · &copy; {new Date().getFullYear()}.
-                        All rights reserved.
+                        Built by Afif Rohul · &copy; {new Date().getFullYear()} All rights reserved.
                     </p>
                 </div>
                 <div className="flex flex-col gap-4">
