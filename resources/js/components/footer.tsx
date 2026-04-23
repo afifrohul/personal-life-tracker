e7efdea3a98c403b41d667c2cbb4eab1e7cc9c73
afifrohul <afifmemyself22@gmail.com>
2026-04-23 09:50:12 +0700

    update footer

--- resources/js/components/footer.tsx
+++ resources/js/components/footer.tsx
@@ -1,4 +1,5 @@
 import { Separator } from '@/components/ui/separator';
+import { FaGlobe } from "react-icons/fa";
 import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { RiInstagramFill } from 'react-icons/ri';
 
@@ -9,11 +10,20 @@ export default function Footer() {
             <div className="flex flex-col items-center justify-between gap-4 p-4 lg:flex-row">
                 <div className="flex items-center justify-center gap-2 lg:justify-start">
                     <p className="text-xs italic">
-                        Built by Afif Rohul · &copy; {new Date().getFullYear()} All rights reserved.
+                        Built by Afif Rohul · &copy; {new Date().getFullYear()}{' '}
+                        All rights reserved.
                     </p>
                 </div>
                 <div className="flex flex-col gap-4">
                     <div className="flex gap-4">
+                        <a
+                            href="https://afifrohul.vercel.app/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-gray-600 transition-all duration-150 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400"
+                        >
+                            <FaGlobe />
+                        </a>
                         <a
                             href="https://www.instagram.com/afif.rohul/"
                             target="_blank"
