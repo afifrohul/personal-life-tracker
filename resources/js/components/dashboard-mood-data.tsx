7c5afe3c3743992934f679e9b0474f0a89a5dfe8
afifrohul <afifmemyself22@gmail.com>
2026-09-16 18:28:51 +0700

    fix: typo in dashboard mood data component

--- resources/js/components/dashboard-mood-data.tsx
+++ resources/js/components/dashboard-mood-data.tsx
@@ -37,27 +37,27 @@ export default function DashboardMoodData({
             />
             <DashboardInfo
                 icon={<LuAngry className="text-rose-500" />}
-                desc="Total Bad Mood:"
+                desc="Total Bad Mood Log(s):"
                 data={`${badMoodCount} Log(s)`}
             />
             <DashboardInfo
                 icon={<LuFrown className="text-amber-500" />}
-                desc="Total Not Good Mood:"
+                desc="Total Not Good Mood Log(s):"
                 data={`${notGoodMoodCount} Log(s)`}
             />
             <DashboardInfo
                 icon={<LuMeh className="text-yellow-500" />}
-                desc="Total Okay Mood:"
+                desc="Total Okay Mood Log(s):"
                 data={`${okayMoodCount} Log(s)`}
             />
             <DashboardInfo
                 icon={<LuSmile className="text-green-500" />}
-                desc="Total Good Mood:"
+                desc="Total Good Mood Log(s):"
                 data={`${goodMoodCount} Log(s)`}
             />
             <DashboardInfo
                 icon={<LuSmilePlus className="text-teal-500" />}
-                desc="Total Great Mood:"
+                desc="Total Great Mood Log(s):"
                 data={`${greatMoodCount} Log(s)`}
             />
         </div>
